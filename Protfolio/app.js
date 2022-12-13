bb73b3ce7ae2382daf7d5fab8343bf629d173051
Abhishek-RX <abhishek062000@gmail.com>
2022-12-14 00:14:15 +0530

    A

---
+++ Protfolio/app.js
@@ -0,0 +1,22 @@
+// let links = document.querySelectorAll('.link');
+// let  sections = document.querySelectorAll('section');
+
+// let activeLink = 0;
+
+// links.forEach((link, i) => {
+//     link.addEventListener('click', () => {
+//         if(activeLink != i){
+//             links[activeLink].classList.remove('active');
+//             link.classList.add('active');
+//             sections[activeLink].classList.remove('active');
+
+//             setTimeout(() => {
+//                 activeLink = i;
+//                 sections[i].classList.add('active');
+//             }, 1000);
+//         }
+//     })
+// })
+
+
+
